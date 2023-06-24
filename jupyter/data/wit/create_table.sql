afde5e4714cccced57c9d2e236fcdffa40ab513b
Di Qi <di@racer.app>
2023-06-24 00:25:32 -0700

    Add indexes

--- jupyter/data/wit/create_table.sql
+++ jupyter/data/wit/create_table.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS tsv_data (
   language TEXT,
   page_url TEXT,
   image_url TEXT,
-  image_url_ai VECTOR(512),
+  image_url_ai1 VECTOR(512),
+  image_url_ai2 VECTOR(512),
   page_title TEXT,
   section_title TEXT,
   hierarchical_section_title TEXT,
@@ -17,6 +18,10 @@ CREATE TABLE IF NOT EXISTS tsv_data (
   attribution_passes_lang_id BOOLEAN,
   page_changed_recently BOOLEAN,
   context_page_description TEXT,
-  context_page_description_ai VECTOR(768),
+  context_page_description_ai1 VECTOR(768),
+  context_page_description_ai2 VECTOR(768),
   context_section_description TEXT
 );
+
+CREATE INDEX ON tsv_data USING ivfflat (image_url_ai2 vector_l2_ops) WITH (lists = 100);
+CREATE INDEX ON tsv_data USING ivfflat (context_page_description_ai2 vector_l2_ops) WITH (lists = 100);
