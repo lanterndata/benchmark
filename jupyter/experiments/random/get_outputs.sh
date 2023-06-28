498c50fe99b1b52ee8b626d856c86d66bebd1e66
Di Qi <di@racer.app>
2023-06-28 17:34:12 -0400

    Edit N K D for experiments

--- jupyter/experiments/random/get_outputs.sh
+++ jupyter/experiments/random/get_outputs.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 # Define values for N, K, D
-K_values=(1 2 4 8 16 32 64)
-N_values=(100000 500000 1000000 5000000 10000000)
-D_values=(5 10 50 100 200 400 800)
+K_values=(2 4 8 16 32 64)
+N_values=(100000 200000 400000 800000 1600000)
+D_values=(5 10 50 100 200 400)
 
-# Loop over p1 and p2 values
+# Loop over N, D, K values
 for N in "${N_values[@]}"; do
   ./create_data.sh "$N"
   for D in "${D_values[@]}"; do
     for K in "${K_values[@]}"; do
       ./run_pgbench.sh "$N" "$D" "$K"
-      echo "Finished pgbench with N=$N, D=$D, K=$K"
     done
   done
   ./destroy_data.sh
