60b778977f607dedf800ab9c703359bdef7e1010
Di Qi <di@racer.app>
2023-07-05 10:00:55 -0400

    Create tables

--- experiments/sift/create_tables.sql
+++ experiments/sift/create_tables.sql
@@ -1,27 +1,32 @@
+DROP TABLE IF EXISTS sift_base10k;
+DROP TABLE IF EXISTS sift_base1m;
+DROP TABLE IF EXISTS gist_base1m;
+-- DROP TABLE IF EXISTS sift_base1b;
+
 CREATE TABLE IF NOT EXISTS sift_base10k (
   id SERIAL PRIMARY KEY,
   v VECTOR(128)
 );
 
-\COPY sift_base10k (v) FROM '/Users/diqi/postvec/benchmark/jupyter/data/sift/sift_base10k.csv' WITH csv;
+\COPY sift_base10k (v) FROM '/app/data/sift/sift_base10k.csv' WITH csv;
 
 CREATE TABLE IF NOT EXISTS sift_base1m (
   id SERIAL PRIMARY KEY,
   v VECTOR(128)
 );
 
-\COPY sift_base1m (v) FROM '/Users/diqi/postvec/benchmark/jupyter/data/sift/sift_base1m.csv' WITH csv;
+\COPY sift_base1m (v) FROM '/app/data/sift/sift_base1m.csv' WITH csv;
 
--- CREATE TABLE IF NOT EXISTS gist_base1m (
---   id SERIAL PRIMARY KEY,
---   v VECTOR(960)
--- );
+CREATE TABLE IF NOT EXISTS gist_base1m (
+  id SERIAL PRIMARY KEY,
+  v VECTOR(960)
+);
 
--- \COPY gist_base1m (v) FROM '/jupyter/data/sift/gist_base1m.csv' WITH csv;
+\COPY gist_base1m (v) FROM '/app/data/gist/gist_base1m.csv' WITH csv;
 
 -- CREATE TABLE IF NOT EXISTS sift_base1b (
 --   id SERIAL PRIMARY KEY,
 --   v VECTOR(128)
 -- );
 
--- \COPY sift_base1b (v) FROM '/jupyter/data/sift/sift_base1b.csv' WITH csv;
+-- \COPY sift_base1b (v) FROM '/app/data/sift/sift_base1b.csv' WITH csv;
--- experiments/sift/create_tables_recall.sql
+++ experiments/sift/create_tables_recall.sql
@@ -1,32 +1,48 @@
-DROP TABLE sift_query10k;
-DROP TABLE sift_query1m;
-DROP TABLE sift_truth10k;
-DROP TABLE sift_truth1m;
+DROP TABLE IF EXISTS sift_query10k;
+DROP TABLE IF EXISTS sift_query1m;
+DROP TABLE IF EXISTS gist_query1m;
+DROP TABLE IF EXISTS sift_truth10k;
+DROP TABLE IF EXISTS sift_truth1m;
+DROP TABLE IF EXISTS gist_truth1m;
 
 CREATE TABLE IF NOT EXISTS sift_query10k (
   id SERIAL PRIMARY KEY,
   v VECTOR(128)
 );
 
-\COPY sift_query10k (v) FROM '/Users/diqi/postvec/benchmark/jupyter/data/sift/siftsmall_query.csv' WITH csv;
+\COPY sift_query10k (v) FROM '/app/data/sift/siftsmall_query.csv' WITH csv;
 
 CREATE TABLE IF NOT EXISTS sift_query1m (
   id SERIAL PRIMARY KEY,
   v VECTOR(128)
 );
 
-\COPY sift_query1m (v) FROM '/Users/diqi/postvec/benchmark/jupyter/data/sift/sift_query.csv' WITH csv;
+\COPY sift_query1m (v) FROM '/app/data/sift/sift_query.csv' WITH csv;
 
 CREATE TABLE IF NOT EXISTS sift_truth10k (
   id SERIAL PRIMARY KEY,
   indices INTEGER[]
 );
 
-\COPY sift_truth10k (indices) FROM '/Users/diqi/postvec/benchmark/jupyter/data/sift/siftsmall_groundtruth.csv' WITH csv;
+\COPY sift_truth10k (indices) FROM '/app/data/sift/siftsmall_groundtruth.csv' WITH csv;
 
 CREATE TABLE IF NOT EXISTS sift_truth1m (
   id SERIAL PRIMARY KEY,
   indices INTEGER[]
 );
 
-\COPY sift_truth1m (indices) FROM '/Users/diqi/postvec/benchmark/jupyter/data/sift/sift_groundtruth.csv' WITH csv;
+\COPY sift_truth1m (indices) FROM '/app/data/sift/sift_groundtruth.csv' WITH csv;
+
+CREATE TABLE IF NOT EXISTS gist_query1m (
+  id SERIAL PRIMARY KEY,
+  v VECTOR(960)
+);
+
+\COPY gist_query1m (v) FROM '/app/data/gist/gist_query.csv' WITH csv;
+
+CREATE TABLE IF NOT EXISTS gist_truth1m (
+  id SERIAL PRIMARY KEY,
+  indices INTEGER[]
+);
+
+\COPY gist_truth1m (indices) FROM '/app/data/gist/gist_groundtruth.csv' WITH csv;
