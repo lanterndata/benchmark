30f958b5dd141b7eec655db9ef9b36539a3da3c1
Di Qi <dqi@alumni.princeton.edu>
2023-07-14 01:47:55 +0000

    Remove unused index SQL scripts

--- experiments/sift/scripts/create_indices.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX IF NOT EXISTS sift_base10k_index ON sift_base10k USING ivfflat (v vector_l2_ops) WITH (lists = 100);
-CREATE INDEX IF NOT EXISTS sift_base1m_index ON sift_base1m USING ivfflat (v vector_l2_ops) WITH (lists = 100);
--- CREATE INDEX gist_base1m_index ON gist_base1m USING ivfflat (v vector_l2_ops) WITH (lists = 100);
--- CREATE INDEX sift_base1b_index ON sift_base1b USING ivfflat (v vector_l2_ops) WITH (lists = 100);
--- experiments/sift/scripts/create_indices_derived.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX IF NOT EXISTS sift_base100k_index ON sift_base100k USING ivfflat (v vector_l2_ops) WITH (lists = 100);
-CREATE INDEX IF NOT EXISTS sift_base200k_index ON sift_base200k USING ivfflat (v vector_l2_ops) WITH (lists = 100);
-CREATE INDEX IF NOT EXISTS sift_base400k_index ON sift_base400k USING ivfflat (v vector_l2_ops) WITH (lists = 100);
-CREATE INDEX IF NOT EXISTS sift_base600k_index ON sift_base600k USING ivfflat (v vector_l2_ops) WITH (lists = 100);
-CREATE INDEX IF NOT EXISTS sift_base800k_index ON sift_base800k USING ivfflat (v vector_l2_ops) WITH (lists = 100);
