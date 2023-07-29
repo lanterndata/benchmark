d10c5d49ca2c94debb83838e342ea9af8d4edcf7
Di Qi <dqi@alumni.princeton.edu>
2023-07-29 21:53:11 +0000

    Delete de-prioritized experiments

--- experiments/random/delete_tables.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS random_table_2m;
-DROP TABLE IF EXISTS random_table_1m;
-DROP TABLE IF EXISTS random_table_750k;
-DROP TABLE IF EXISTS random_table_500k;
-DROP TABLE IF EXISTS random_table_250k;
