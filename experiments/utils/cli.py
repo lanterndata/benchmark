1838c66aaf081b9b034294bedce7199a2da09cdb
Di Qi <di@lantern.dev>
2023-08-18 08:44:01 -0700

    Fix bugs with CLI (#10)
    * fix: cli args
    
    * fix: cli args for select experiment(s)
    
    * fix: cli args for select bulk

--- experiments/utils/cli.py
+++ experiments/utils/cli.py
@@ -1,13 +1,15 @@
 import argparse
-from .constants import VALID_DATASETS, VALID_EXTENSIONS, NO_INDEX_METRICS, VALID_INDEX_PARAMS, SUGGESTED_K_VALUES, Extension, Dataset
+from .constants import VALID_DATASETS, VALID_DATASET_SIZES, VALID_EXTENSIONS, VALID_INDEX_PARAMS, SUGGESTED_K_VALUES, Extension, Dataset
 
 
-def parse_args(description, args):
+def parse_args(description, args, allow_no_index=False):
     parser = argparse.ArgumentParser(description=description)
 
     if 'extension' in args:
-        valid_extensions = list(Extension) if Extension(
-            description) in NO_INDEX_METRICS else VALID_EXTENSIONS
+        if allow_no_index:
+            valid_extensions = [e.value for e in Extension]
+        else:
+            valid_extensions = [e.value for e in VALID_EXTENSIONS]
         parser.add_argument(
             '--extension',
             type=str, choices=valid_extensions, required=True, help='Extension type')
@@ -19,7 +21,7 @@ def parse_args(description, args):
 
     parser.add_argument(
         "--dataset",
-        type=str, choices=list(VALID_DATASETS), required=True, help="Dataset name")
+        type=str, choices=[d for d in VALID_DATASETS], required=True, help="Dataset name")
 
     if 'N' in args:
         parser.add_argument(
@@ -38,7 +40,7 @@ def parse_args(description, args):
     if 'extension' in args:
         extension = Extension(parsed_args.extension)
     if 'N' in parsed_args:
-        valid_datasets = VALID_DATASETS[dataset]
+        valid_datasets = VALID_DATASET_SIZES[dataset]
         N_values = parsed_args.N or valid_datasets
         if any(N not in valid_datasets for N in N_values):
             valid_sizes = ', '.join(valid_datasets)
@@ -48,7 +50,7 @@ def parse_args(description, args):
         K = parsed_args.K or SUGGESTED_K_VALUES
 
     index_params = None
-    if extension is not None:
+    if extension in VALID_INDEX_PARAMS:
         index_params = {
             param: getattr(parsed_args, param)
             for param in VALID_INDEX_PARAMS[extension] if getattr(parsed_args, param) is not None}
