8b28e757e6641958bb27387e8cbff8ea2412aa02
Di Qi <di@lantern.dev>
2023-09-09 18:16:49 +0000

    Support printing markdown

--- external/show_benchmarks.py
+++ external/show_benchmarks.py
@@ -1,14 +1,26 @@
+import argparse
 from typing import List, Tuple
 from core.utils.constants import Metric
 from external.utils import cli
 from external.utils.get_benchmarks import get_benchmarks
 
 
-def print_benchmarks(benchmarks: List[Tuple[Metric, str, str]]):
+def add_arguments(parser: argparse.ArgumentParser) -> None:
+    """
+    Add specific arguments for the show_benchmarks script.
+    """
+    parser.add_argument('--markdown', action='store_true',
+                        help='Print benchmarks in markdown format')
+
+
+def print_benchmarks(benchmarks: List[Tuple[Metric, str, str]], markdown=False):
     divider_length = 80
     divider_line = "-" * divider_length
 
-    print(divider_line)
+    if markdown:
+        print("# Benchmarks")
+    else:
+        print(divider_line)
     print("| %-21s | %18s | %18s | %10s |" %
           ("metric", "old", "new", "pct change"))
     print(divider_line)
@@ -18,19 +30,20 @@ def print_benchmarks(benchmarks: List[Tuple[Metric, str, str]]):
             "%.3f" % float(old_value)
         display_new_value = "-" if new_value is None else \
             "%.3f" % float(new_value)
-        pct_change = "-" if new_value is None or old_value is None else \
-            (float(new_value) - float(old_value)) / float(old_value) * 100
-        display_pct_change = "%.2f%%" % pct_change
+        display_pct_change = "-" if new_value is None or old_value is None else \
+            "%.2f%%" % (float(new_value) - float(old_value)) / \
+            float(old_value) * 100
         data = (metric.value, display_old_value,
                 display_new_value, display_pct_change)
         print("| %-21s | %18s | %18s | %10s |" % data)
 
-    print(divider_line)
+    if not markdown:
+        print(divider_line)
 
 
 if __name__ == "__main__":
-    extension, index_params, dataset, N, K = cli.get_args(
-        "show benchmark results for tests or CI/CD")
+    extension, index_params, dataset, N, K, markdown = cli.get_args(
+        "show benchmark results for tests or CI/CD", add_arguments, ['markdown'])
     benchmarks = get_benchmarks(
         extension, index_params, dataset, N, K, return_old=True)
-    print_benchmarks(benchmarks)
+    print_benchmarks(benchmarks, markdown=markdown)
--- external/utils/cli.py
+++ external/utils/cli.py
@@ -1,15 +1,19 @@
 import argparse
+from typing import Optional, Callable, List
 from core.utils.constants import Extension, Dataset
 from core.utils import cli
 
-def get_args(descrption):
+
+def get_args(description, add_arguments: Optional[Callable[[argparse.ArgumentParser], None]] = None, additional_args: List[str] = []):
     # Set up parser
-    parser = argparse.ArgumentParser(description=descrption)
+    parser = argparse.ArgumentParser(description=description)
     cli.add_extension(parser)
     cli.add_index_params(parser)
     cli.add_dataset(parser)
     cli.add_N(parser)
     cli.add_K(parser)
+    if add_arguments is not None:
+        add_arguments(parser)
 
     # Parse arguments
     parsed_args = parser.parse_args()
@@ -20,4 +24,4 @@ def get_args(descrption):
     cli.validate_N(parser, dataset, N)
     K = parsed_args.K
 
-    return extension, index_params, dataset, N, K
+    return extension, index_params, dataset, N, K, *[getattr(parsed_args, arg) for arg in additional_args]
